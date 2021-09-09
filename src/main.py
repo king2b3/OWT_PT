956908a065ab2d0947e4dd412e6989fb6a5084d4
Bayley King <king2b3@mail.uc.edu>
2021-09-08 23:13:41 -0400

    working system

--- src/main.py
+++ src/main.py
@@ -15,6 +15,7 @@ for key in master[key]
 """
 
 from user import User
+import csv
 
 def exists(name) -> bool:
     """Checks if a user already exists in the database with that name"""
@@ -29,16 +30,24 @@ def read_data() -> dict:
 
 
 def main():
+    week = 1
     #create master user with correct scores
-    master = User()
-    """
-    #open csv of users
-    for line in csv:
-        create user from line of csv
-        append user to a list
-
-    for u in user:
-        
     
-    """
-    ...
+    with open("weekly_data/week_1_pt.csv") as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        #next(csv_reader)
+        master = True
+        for row in csv_reader:
+            print(f"User {row['Discord Tag? **case sensitive**']}")
+            if master:
+                master_user = User(data=row,week=week)
+                master = False
+            else:
+
+                temp = User(data=row,week=week)
+                #print(row)
+                temp == master_user
+                temp.write()
+
+if __name__ == '__main__':
+    main()
--- src/user.py
+++ src/user.py
@@ -6,58 +6,83 @@
 """
 
 class User:
-    def __init__(self, week) -> None:
-        self.name = None
-        self.tier = None
-        self.team = None
+    def __init__(self, data:dict, week:int) -> None:
+        #saves the needed data AND removes them from the dict
+        self.name = data.pop("Discord Tag? **case sensitive**")
+        self.tier = data.pop("Which tier do you play in? ")
+        self.team = data.pop("What Tranquility team(s) are you part of?")
+        #remove the timestamp
+        #print(data)
+        #data.pop("Timestamp")
+        data.pop("\ufeffTimestep")
         self.week = week
-        # set name lowercase
-        self.picks = {
-            'hm1' : [0,0],
-            'hm2' : [0,0],
-            'hm3' : [0,0],
-            'hm4' : [0,0],
-            'hm5' : [0,0],
-            'hm6' : [0,0],
-            'hm7' : [0,0],
-            'hm8' : [0,0],
-            'hm9' : [0,0],
-            'hm10' : [0,0],
-            'hm11' : [0,0],
-            'hm12' : [0,0],
-            # Discord Tier Matches
-            'dm1' : [0,0],
-            'dm2' : [0,0],
-            'dm3' : [0,0],
-            'dm4' : [0,0],
-            'dm5' : [0,0],
-            'dm6' : [0,0],
-            'dm7' : [0,0],
-            'dm8' : [0,0],
-            'dm9' : [0,0],
-            'dm10' : [0,0],
-            'dm11' : [0,0],
-            'dm12' : [0,0],
-            # Trance Tier Matches
-            'tm1' : [0,0],
-            'tm2' : [0,0],
-            'tm3' : [0,0],
-            'tm4' : [0,0],
-            'tm5' : [0,0],
-            'tm6' : [0,0],
-            'tm7' : [0,0],
-            'tm8' : [0,0],
-            'tm9' : [0,0],
-            'tm10' : [0,0],
-            'tm11' : [0,0],
-            'tm12' : [0,0]
-        }
-    
+        #converts the reminaing dict into list of picks
+        self.picks = list(data.values())
+        self.score = 0
+        self.check_vals()
+                
     def check_vals(self) -> None:
-
-        ...
+        """If anything isn't filled out, make into a 0"""
+        for i in range(len(self.picks)):
+            if self.picks[i] == "":
+                self.picks[i] = "0"
     
     def write(self) -> None:
-        #appends csv results to the output sheet
-        #use self.week to write to proper col, self.name for row
-        ...
+        import csv
+        #searches if the user already exists
+        not_exist = True
+        with open("output.csv", "r") as csv_output:
+            reader = csv.reader(csv_output)
+            for user in list(reader):
+                if user[0] == self.name:
+                    print(f"\tUSER FOUND")
+                    temp_list = user
+                    not_exist = False
+                    break
+
+        #re-write to new file because I didn't use Pandas (you should)
+        with open("new_output.csv", "a") as csv_output:
+            output = csv.writer(csv_output)
+            if not_exist:
+                temp_list = [self.name, self.team, self.tier,0,0,0,0,0,0,0,
+                        0,0,0,0,0,0,0]
+                temp_list[self.week+2] = self.score
+
+            else:
+                temp_list[self.week + 2] = self.score
+            
+            output.writerow(temp_list)
+                
+    def __eq__(self, u2) -> None:
+        """u2 is considered the master user
+        set the score of master vs self for the number of correct picks
+        """
+        for i in range(0,len(self.picks),2):
+            self.score += self.match(self.picks[i],self.picks[i+1],
+                    u2.picks[i],u2.picks[i+1])
+    
+    @staticmethod
+    def match(team1_user, team2_user, team1_master, team2_master) -> int:
+        """Returns the score from the match pick"""
+        #don't need to typecast to int because Python is best
+        #check if exactly predicted
+        if ((team1_user == team1_master) and (team2_user == team2_master)):
+            return 3
+        
+        #did the home team win?
+        if team1_master > team2_master:
+            master_win = True
+        else:
+            master_win = False
+
+        #did the user predict the home team to win?
+        if team1_user > team2_user:
+            user_win = True
+        else:
+            user_win = False
+
+        #did the user predict correctly?
+        if master_win == user_win:
+            return 1
+        else:
+             return 0
